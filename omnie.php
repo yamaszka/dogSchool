b0515594d73f8a6a90deb1239ac678a7520037c6
Basia <you@example.com>
2019-05-20 19:12:50 +0200

    position fixed for title and omnie.php RWD

---
+++ omnie.php
@@ -0,0 +1,34 @@
+
+<?php include 'naglowek.txt'; ?>
+
+<div id="obraz">
+<img src="galeria/jaIpsy.jpg" alt="" class="obraz_img"/>
+</div>
+
+<?php include 'srodek.txt'; ?>
+
+<div id="glowna">
+Nazywam się Barbara Czuba.<br>
+Jestem związana z psami od wielu lat. Pierwszy puchar na zawodach Pies Towarzyszący z moją suką cocer spaniel zdobyłam w 2009r.<br><br>
+
+Ukończyłam podyplomowe studia kynologiczne w Szkole Głównej Gospodarstwa Wiejskiego w Warszawie na kierunku "Pies w społeczeństwie. Hodowla i zachowanie" z oceną bardzo dobrą.<br>
+Uczestniczyłam w wielu kursach i seminariach prowadzonych przez znanych i cenionych instruktorów psich sportów.<br><br>
+
+Przez kilka lat czynnie uprawiałam sporty kynologiczne z dwoma sukami rasy border collie. Poszerzając swoje horyzonty przygotowywałam je do wielu konkurencji. Agility, Frisbee, Pies Towarzyszący, Obidience, Dogtrekking, Tropienie, Pasienie. Mamy za sobą kilka sukcesów w postaci zdanych egzaminów Agility, PT (Pies Towarzyszący), kwalifikacji do Mistrzostw Polski Frisbee, III miejsca w Mistrzostwach Polski Dogtrekkingu 2015, III miejsca w Pucharze Polski Dogtrekkingu 2015 oraz II miejsce w Mistrzostwach Polski Dogtrekkingu 2018 na dystansie long. <br>
+Moje psy mają 6 i 8 lat. Trenujemy (wolę te sesje nazywać zabawą) co kilka dni. Obecnie najczęściej można nas spotkać na placu, naszego ukochanego agility, a także na leśnych ścieżkach podczas treningów bikejoring. W wolne wieczory aktywujemy nosy poszukując róznych zapachów. <br><br>
+
+Jestem także, wraz z moimi psami, półfinalistką programu telewizyjnego TVN "Mam Talent" do którego kwalifikację uzyskałam z pokazem nazwanym przeze mnie "kabaretem na dwa psy".<br><br>
+
+Dodatkowo prowadzę hodowlę rasy border collie o profilu sportowym.<br>
+
+Zapraszam na prelekcje, pokazy i szkolenia.<br>
+Chętnie odpowiem na wszelkie pytania dotyczące psów.<br><br>
+
+Zapraszam do obejrzenia kilku filmów:<br>
+- <b>Rysa i Oda sztuczki</b><br> <iframe class="obraz_img" src="https://www.youtube.com/embed/RDZOFF9oS0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br>
+- <b>Rysa i Oda "Nothing is impossible"</b><br> <iframe class="obraz_img" src="https://www.youtube.com/embed/FGiTcO3FvQQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br>
+- <b>"Dzień Dobry TVN"</b><br> <a target="blank" href="https://dziendobry.tvn.pl/a/jakie-sa-rodzaje-edukacyjnych-zabawek-dla-psa?fbclid=IwAR3YfgXICAXiwo313mZVV1W5Dgmtoxo3KFFSSW2lt_aylUiCGPZ_o6gYF0Y">link do filmu</a>
+<br><br>
+</div><!-- glowna -->
+
+<?php include 'stopka.txt'; ?>
